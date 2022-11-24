f2199928f51c12cc8882b12540039bfdf2b0510d
Maelton Lima <mls54@aluno.ifal.edu.br>
2022-11-24 04:36:47 -0300

    meu primeiro app

---
+++ main.py
@@ -0,0 +1,11 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+class MinhaMainInterface(Widget):
+    pass
+
+class MeuPrimeiroApp(App):
+    pass
+
+MeuPrimeiroApp().run()
